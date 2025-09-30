54773a74b0495b1487ae27ef49fce2b0dae8bc6a
Sunny <77166237+bashSunny101@users.noreply.github.com>
2025-09-30 13:19:55 +0530

    Complete 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseString = "";
+    for(let i=word.length; i>=0;i--) {
+        reverseString += word.charAt(i);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
